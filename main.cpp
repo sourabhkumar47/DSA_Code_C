0fd8b321a2eeca3177e6432abf5802db39999a76
Sourabh Kumar <sourabh.47512@gmail.com>
2024-07-13 08:09:54 +0000

    added patterns

--- main.cpp
+++ main.cpp
@@ -1,5 +1,87 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  //equlataral triangle
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++){
+
+  }
+
+  // inverted trainagle number
+  // int n;
+  // cin >> n;
+
+  //  for (int i = 0; i < n; i++) {
+  //    for (int j = 0; j <n- i; j++) {
+  //      cout << j+1;
+  //    }
+  //    cout << " " << endl;
+  //  }
+
+  // // trainagle number
+  // int n;
+  // cin >> n;
+
+  //  for (int i = 0; i < n; i++) {
+  //    for (int j = 0; j < i+1; j++) {
+  //      cout << j+1;
+  //    }
+  //    cout << " " << endl;
+  //  }
+
+  // // half piramid inverted
+  // int n;
+  // cin >> n;
+
+  //  for (int i = 0; i < n; i++) {
+  //    for (int j = 0; j < n-i; j++) {
+  //      cout << "* ";
+  //    }
+  //    cout << " " << endl;
+  //  }
+
+  // // half piramid
+  // int a;
+  // cin >> a;
+
+  // for (int i=0; i < a; i++) {
+  //   for (int j=0; j < i+1; j++){
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // Hollow rec
+  // int n, m;
+  // cout << "Enter rows: " << endl;
+  // cin >> n;
+  // cout << "Enter column: " << endl;
+  // cin >> m;
+
+  // for (int i = 0; i < n; i++) {
+  //   if (i == 0 || i == n - 1) {
+  //     for (int j = 0; j < m; j++) {
+  //       cout << "* ";
+  //     }
+  //     cout << endl;
+  //   } else {
+  //     cout << "* ";
+  //     for (int a = 0; a < m - 2; a++) {
+  //       cout << "  ";
+  //     }
+  //     cout << "* ";
+
+  //     cout << endl;
+  //   }
+  // }
+
+  // rectangle
+  //  for (int i = 0; i < n; i++) {
+  //    for (int j = 0; j < n; j++) {
+  //      cout << "* ";
+  //    }
+  //    cout << " " << endl;
+  //  }
 }

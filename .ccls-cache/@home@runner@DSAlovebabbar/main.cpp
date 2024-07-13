6d478a345d51d564381c329c79e7e95f0b8850fb
Sourabh Kumar <sourabh.47512@gmail.com>
2024-07-13 17:39:03 +0000

    added patterns

--- .ccls-cache/@home@runner@DSAlovebabbar/main.cpp
+++ .ccls-cache/@home@runner@DSAlovebabbar/main.cpp
@@ -2,12 +2,83 @@
 using namespace std;
 
 int main() {
-  //equlataral triangle
-  int n;
-  cin>>n;
-  for(int i=0;i<n;i++){
 
-  }
+  
+  // Diamond
+  // int n;
+  // cin >> n;
+
+  // for (int row = 0; row < n; row++) {
+  //   // upar
+  //   for (int col = 0; col < n - row - 1; col++) {
+  //     cout << " ";
+  //   }
+  //   for (int col = 0; col < 2 * row + 1; col++) {
+  //     if (col == 0 || col == 2 * row) {
+  //       cout << "* ";
+  //     } else {
+  //       cout << " ";
+  //     }
+  //   }
+  //   cout << endl;
+  // }
+  // // niche
+  // for (int row = 0; row < n; row++) {
+  //   for (int col = 0; col < row; col++) {
+  //     cout << " ";
+  //   }
+
+  //   for (int col = 0; col < 2 * n - 2 * row - 1; col++) {
+  //     if (col == 0 || col ==  2 * n - 2 * row - 2) {
+  //       cout << "* ";
+  //     } else {
+  //       cout << " ";
+  //     }
+  //   }
+  //   cout << endl;
+  // }
+
+  // number triangle
+  //  int a;
+  //  cin >> a;
+
+  // for (int i=0; i < a; i++) {
+  //   for(int k=0;k<a-i-1;k++){
+  //     cout << " ";
+  //   }
+  //   for (int j=0; j <i+1; j++){
+  //     cout<<  "i ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // //inverted equlataral triangle
+  // int a;
+  // cin >> a;
+
+  // for (int i=0; i < a; i++) {
+  //   for(int k=0;k<i;k++){
+  //     cout << " ";
+  //   }
+  //   for (int j=0; j < a-i; j++){
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  // equlataral triangle
+  //  int a;
+  //  cin >> a;
+
+  // for (int i=0; i < a; i++) {
+  //   for(int k=0;k<a-i-1;k++){
+  //     cout << " ";
+  //   }
+  //   for (int j=0; j < i+1; j++){
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
 
   // inverted trainagle number
   // int n;

f0020d2f64d16e05aac8bd1ae780dfe6bd6ce3cc
Sourabh Kumar <sourabh.47512@gmail.com>
2024-08-28 01:55:08 +0530

    added binary search

--- Topics/Searching/BinarySearch.cpp
+++ Topics/Searching/BinarySearch.cpp
@@ -1,29 +1,67 @@
 #include <iostream>
 using namespace std;
 
-int binarysearch(int arr[],int size,int target){
-    int start =0;
-    int end = size-1;
+// Simple binary search
+int binarysearch(int arr[], int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
 
-    while(start<=end){
-        int mid = (start+end)/2;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
 
-        if(arr[mid]==target){
+        if (arr[mid] == target)
+        {
             return mid;
-        }else if(target> arr[mid]){
-            start = mid+1;
-        }else{
-            end = mid-1;
+        }
+        else if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
         }
     }
-    return -1;    
+    return -1;
 }
 
-int main(){
-    int arr[]= {2,3,4,5,6,7,8,9,10};
-    int size = 9;
+// First occurance in binary search
+int firstOccurance(int arr[], int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
+    int ans;
 
-    int ans = binarysearch(arr,size,10);
-    cout<<ans;
-    
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10};
+    int size = 11;
+
+    int ans = firstOccurance(arr, size, 9);
+
+    cout << ans+1;
 }

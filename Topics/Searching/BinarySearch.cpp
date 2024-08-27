8f28b4e96652a89c77ccd40fffe4bb2565c4192c
Sourabh Kumar <sourabh.47512@gmail.com>
2024-08-28 02:17:38 +0530

    find no of coourance

--- Topics/Searching/BinarySearch.cpp
+++ Topics/Searching/BinarySearch.cpp
@@ -56,12 +56,54 @@ int firstOccurance(int arr[], int size, int target)
     return ans;
 }
 
+// Last occurance in binary search
+int lastOccurance(int arr[], int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
+    int ans;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+// Count total number of occurance of any repeating number
+// we know array is sorted so just find first and last occurance and add 1 to it;
+
+int countOccurance(int arr[], int size, int target)
+{
+    int first = firstOccurance(arr, size, target);
+    int last = lastOccurance(arr, size, target);
+
+    int total = last - first + 1;
+
+    return total;
+}
+
 int main()
 {
     int arr[] = {2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10};
     int size = 11;
 
-    int ans = firstOccurance(arr, size, 9);
+    int ans = countOccurance(arr, size, 4);
 
-    cout << ans+1;
+    cout << "Total occurance: ";
 }

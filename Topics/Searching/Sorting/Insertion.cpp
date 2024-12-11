ac54fa95e8ef8c7f29326f5eb51b9495a34598c8
Sourabh Kumar <sourabh.47512@gmail.com>
2024-12-11 15:11:55 +0530

    insertion sort

--- Topics/Searching/Sorting/Insertion.cpp
+++ Topics/Searching/Sorting/Insertion.cpp
@@ -2,8 +2,20 @@
 #include <vector>
 using namespace std;
 
+// in insertion sort, we iterate over the array and for each element
+// we find its correct position in the sorted array
+// and insert it there
+// we user 2 pointers i and j
+// i is the current element and j is the element before i
+// we keep on moving j to the left until we find the correct position for i
+// and then insert i there
+
+// time complexity of insertion sort is O(n^2) in worst case
+// and O(n) in best case
+// space complexity is O(1)
 void insertion(vector<int> &arr)
 {
+    // i is always start from 2nd value as previous value is indicated by j
     for (int i = 1; i < arr.size(); i++)
     {
         int key = arr[i];
